c67839211d338ec5af9485e9f27c828d7b8881f9
印哲 莊 <q0963212511@gmail.com>
2024-03-25 00:06:22 +0800

    sleeping

--- my-app/src/renderer/src/views/Edit.vue
+++ my-app/src/renderer/src/views/Edit.vue
@@ -1,17 +1,53 @@
 <script setup lang="ts">
-import { toRefs, reactive,ref, onMounted } from 'vue'
+
+import { toRefs, reactive, ref, onMounted } from 'vue'
 import { useWindowSize } from "@vueuse/core";
-// import App from './Edit.vue';
-// const app = createApp(App);
-
-
-
 import listen from "../picture/listen.png"
 import store from "../picture/store.png"
 import "../assets/edit.css"
 
-const inconlist = Object.values(import.meta.glob('../picture/scene/*',{eager:true})).map((v:any) => v.default);
+const inconlist = Object.values(import.meta.glob('../picture/scene/*', { eager: true })).map((v: any) => v.default);
 
+import { app, protocol, net, BrowserWindow, ipcRenderer } from 'electron'
+import path from 'node:path'
+
+// protocol.registerSchemesAsPrivileged([
+//   {
+//     scheme: 'animation',
+//     privileges: {
+//       bypassCSP: true,
+//       stream: true,
+//     }
+//   }
+// ])
+// function createWindow() {
+//   // Create the browser window.
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: false,
+//       // preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
+
+//   // and load the index.html of the app.
+//   mainWindow.loadFile('index.html')
+
+//   app.whenReady().then(() => {
+//     protocol.handle('animation', function (request) {
+//       console.log('request.url', request.url)
+//       return net.fetch('file://' + request.url.slice('animation://'.length))
+//     })
+
+//     createWindow()
+
+//     app.on('activate', function () {
+
+//       if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//     })
+//   })
+// }
 // import "https://unpkg.com/open-props";
 // import "https://unpkg.com/open-props/normalize.min.css";
 const state = reactive({
@@ -27,84 +63,122 @@ const state = reactive({
     { href: '#', id: 'store', icon: "", alt: 'Services', content: 'Demo' },
   ],
   windows: [
-    { id: 'window1', number: 1 },
-    { id: 'window2', number: 2 },
-    { id: 'window3', number: 3 },
-    { id: 'window4', number: 4 },
+    { id: 'window1', number: 1, clicked: false, color: 'rgb(255,255,255)' },
+    { id: 'window2', number: 2, clicked: false, color: 'rgb(255,255,255)' },
+    { id: 'window3', number: 3, clicked: false, color: 'rgb(255,255,255)' },
+    { id: 'window4', number: 4, clicked: false, color: 'rgb(255,255,255)' },
   ],
   timeSettings: [
-    { label: '場景開始時間', placeholder: '00:00:00', checkboxValue: '1', checkboxName: 'time1', checkboxId: 'time1' },
-    { label: '場景結束時間', placeholder: '00:00:00', checkboxValue: '1', checkboxName: 'time2', checkboxId: 'time2' },
-    { label: 'AD開始時間', placeholder: '00:00:00', checkboxValue: '1', checkboxName: 'time3', checkboxId: 'time3' },
+    { label: '場景開始時間', placeholder: '', checkboxValue: '1', checkboxName: 'time1', checkboxId: 'time1' },
+    { label: '場景開始時間', placeholder: '', checkboxValue: '1', checkboxName: 'time2', checkboxId: 'time2' },
+    { label: '場景開始時間', placeholder: '', checkboxValue: '1', checkboxName: 'time3', checkboxId: 'time3' },
   ],
 })
 const { tools, windows, timeSettings } = toRefs(state)
 // const IMAGE = require.context('../picture/scene', false, /\.png$/)
-const changeAllWindowColors = () => {
-  const windows = document.querySelectorAll('.window')
-  windows.forEach(window => {
-    window.style.backgroundColor = 'rgb(255,255,255)'
-  })
-}
+const toggleWindow = (window) => {
+  for (let i = 0; i < windows.value.length; i++) {
+    windows.value[i].color = 'rgb(255,255,255)'
+  }
+  window.color = '#282828';
 
+};
+
+
+
+
+
+// const a = window.addEventListener('resize', function () {
+//   var width = window.innerWidth;
+
+//   if (width <= 600) {
+//     const topLeftElement = document.querySelector('.top__left');
+//     if (topLeftElement) {
+//       topLeftElement.style.minWidth = '300px';
+//     }
+//     const topMiddleElement = document.querySelector('.top__middle');
+//     if (topMiddleElement) {
+//       topMiddleElement.style.minWidth = '300px';
+//     }
+//     const topRightElement = document.querySelector('.top__right');
+//     if (topRightElement) {
+//       topRightElement.style.minWidth = '200px';
+//     }
+
+//   } else {
+//     const topLeftElement = document.querySelector('.top__left');
+//     if (topLeftElement) {
+//       topLeftElement.style.minWidth = '0px';
+//     }
+//     const topMiddleElement = document.querySelector('.top__middle');
+//     if (topMiddleElement) {
+//       topMiddleElement.style.minWidth = '0px';
+//     }
+//     const topRightElement = document.querySelector('.top__right');
+//     if (topRightElement) {
+//       topRightElement.style.minWidth = '200px';
+//     }
+//   }});
 </script>
-
 <template>
   <div class="content">
+    <div class="title">甚麼時候可以睡覺</div>
+    <hr>
     <div class="top">
       <div class="top__left">
-        <div class="edit_ad">
-          <div class="ad_window">
-            <!-- <div class="outside_ad_tool">
-              <div class="outside_tool_item" v-for="(tool, index) in tools" :key="index">
-                <a :href="tool.link" :id="tool.id" class="outside_tool__link">
-                  <img :src="tool.image" :alt="tool.text" class="outside_tool_icon">
-                  <span class="tooltiptext">{{ tool.text }}</span>
-                </a>
-              </div>
-            </div> -->
-            <div class="wrpaer_ad">
-              <div class="new_window">
-                <div class="window" v-for="window in windows" :key="window.id" :id="window.id"
-                  @click="changeAllWindowColors">
-                  <span>{{ window.number }}</span>
-                </div>
-              </div>
-              <div class="write_ad">
-                <form action="/test.aspx" method="post">
-                  <textarea class="textarea_size" name="" id="" cols="" rows="" style="resize:none;"
-                    placeholder="請填入口述影像"></textarea>
-                </form>
-              </div>
-              <div class="ad_tool">
-                <button class="btn">
-                  <span>STORE</span>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="ad_content_wrpaer">
-            <div class="ad_time_addcontent" v-for="(timeSetting, index) in timeSettings" :key="index">
-              <div class="ad_content">
-                <span>{{ timeSetting.label }}</span>
-              </div>
-              <input type="text" class="ad_time" :placeholder="timeSetting.placeholder">
-              <input type="checkbox" :value="timeSetting.checkboxValue" :name="timeSetting.checkboxName"
-                :id="timeSetting.checkboxId" class="checkbox">
-            </div>
-          </div>
+        <div class="left_title">影片素材</div>
+        <div class="left_container">
         </div>
       </div>
+      <div class="top__middle">
+        <video src="#"></video>
+      </div>
       <div class="top__right">
-        <div class="top__right__user">
-          <video src="https://www.youtube.com/watch?v=hiCnxbwO5-M" width="650" controls></video>
+        <div class="right_title">
+          <span>AD 參數設定</span>
+        </div>
+        <div class="ad_content_wrpaer">
+          <div class="ad_time_addcontent" v-for="(timeSetting, index) in timeSettings" :key="index">
+            <input type="text" class="ad_time" :placeholder="timeSetting.placeholder" required>
+            <div class="underline"></div>
+            <label>{{ timeSetting.label }}</label>
+          </div>
+        </div>
+        <hr>
+        <div class="wrpaer_ad">
+          <div class="new_window">
+            <div class="window" v-for="window in windows" :key="window.id" :id="window.id"
+              :style="{ backgroundColor: window.color }" @click="toggleWindow(window)">
+              <span class="number">{{ window.number }}</span>
+            </div>
+          </div>
+          <hr>
+          <hr>
+          <hr>
+          <div class="write_ad">
+            <form action="/test.aspx" method="post">
+              <textarea class="textarea_size" name="" id="" cols="" rows="" style="resize:none;"
+                placeholder=""></textarea>
+            </form>
+          </div>
+          <div class="ad_tool">
+            <button class="btn">
+              <span>STORE</span>
+            </button>
+            <button class="btn">
+              <span>STORE</span>
+            </button>
+            <button class="btn">
+              <span>STORE</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
     <div class="down">
       <div class="media-scroller snaps-inline">
         <div class="scrollmenu">
-          <img v-for="(item , i) in inconlist" :key ="i" :src="item" >
+          <img v-for="(item, i) in inconlist" :key="i" :src="item">
         </div>
       </div>
 
