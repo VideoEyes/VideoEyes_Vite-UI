b9f11814e6ca3fbdeddafdafe66f5236779d57cb
SulfurStar <35754994+SulfurStar@users.noreply.github.com>
2024-05-07 00:01:51 +0800

    pyscenedetect更新(自動抓fps)

--- pySceneDetect/main.py
+++ pySceneDetect/main.py
@@ -1,18 +1,25 @@
-#usage : python main.py [video_dir] [json_dir] [fps] [min_scene_length_seconds] [output_clips_dir]
+#usage : python main.py [video_dir] [json_dir] [min_scene_length_seconds] [output_clips_folder]
 
 # from csv import writer
 from scenedetect import SceneManager, open_video, AdaptiveDetector ,split_video_ffmpeg #ContentDetector , ThresholdDetector
 # from scenedetect.scene_manager import save_images
 from sys import argv
 from json import dump
+from moviepy.editor import VideoFileClip
 
 # 第一個參數是腳本名稱本身
 # 從第二個參數開始是用戶提供的參數
 script_name = argv[0]
 arguments = argv[1:]
 
-min_scene_length_seconds = int(arguments[3])
-fps = int(arguments[2])
+input_video_path = arguments[0] #"D:\\meowVue\\pySceneDetect\\vid\orig\\net.mp4"
+output_json_path = arguments[1]
+min_scene_length_seconds = int(arguments[2])
+output_videos_path = arguments[3]
+
+clip = VideoFileClip(input_video_path)
+fps = clip.fps
+# print("FPS:", fps)
 
 # find_scenes 函式會回傳一個影片中的所有場景(list of tuple containing start and end timecode in class [Timecode])
 def find_scenes(video_path, threshold=12.0):
@@ -46,17 +53,11 @@ def save_scenes_to_json(scene_list, output_file):
         outer_dict[i] = inner_dict
     with open(output_file, "w") as json_file:
         dump(outer_dict, json_file, indent=4)
-        
-
-input_video_path = arguments[0] #"D:\\meowVue\\pySceneDetect\\vid\orig\\net.mp4"
-output_json_path = arguments[1]
-output_videos_path = arguments[4]
 
 try:
     scene_list = find_scenes(input_video_path)
     save_scenes_to_json(scene_list, output_json_path)
     split_video_ffmpeg(input_video_path, scene_list, output_dir=output_videos_path)
-    # split_into_video(scene_list,input_video_path,output_videos_path)
     # video = open_video(videoPath)
     # save_images(scene_list, video, output_dir=output_dir, num_images=1)
     print("Done")
