1419e0519fdb602be6e44cfcdf84d11e5d1966e7
0rain0 <rain0936@gmail.com>
2024-08-02 16:30:27 +0800

    gemini summarize python

--- Haystack/haystack_gemini_video_AD_summarize.py
+++ Haystack/haystack_gemini_video_AD_summarize.py
@@ -1,35 +1,32 @@
-# usage: python haystack_gemini_video_summarize.py [video_path] [output_json_path]
-
 from haystack_integrations.components.generators.google_vertex import VertexAIGeminiGenerator
 from vertexai.generative_models import Part
-from typing import List
-from pydantic import BaseModel, validator
-from datetime import datetime
 import json
+import random
 import pydantic
 from pydantic import ValidationError
 from typing import Optional, List
+from colorama import Fore
 from haystack import component
 import re
+from typing import List
+from pydantic import BaseModel, validator
 from haystack.components.builders import PromptBuilder
-from google.cloud import storage
-from haystack import Pipeline
 from sys import argv
 
 class audioDescription(BaseModel):
-    time: datetime
-    content: str
-    @validator('time', pre=True)
-    def parse_time(cls, value):
-        return datetime.strptime(value, "%H:%M:%S.%f")
+    Audiodescription: str
 
-class audioDescriptionList(BaseModel):
-    audioDescriptionList: List[audioDescription]
+json_schema = audioDescription.schema_json()
 
+class videoSummary(BaseModel):
+    who: str
+    what: str
+    when: str
+    where: str
+    how: str
+    why: str
 
-json_schema = audioDescriptionList.schema_json(indent=2)
-
-
+summary_schema = videoSummary.schema_json(indent=2)
 
 @component
 class OutputValidator:
@@ -37,12 +34,10 @@ class OutputValidator:
         self.pydantic_model = pydantic_model
         self.iteration_counter = 0
 
-    # Define the component output
     @component.output_types(valid_replies=List[str], invalid_replies=Optional[List[str]], error_message=Optional[str])
     def run(self, replies: List[str]):
         self.iteration_counter += 1
 
-        # Try to parse the LLM's reply
         try:
             json_match = re.search(r'```json\s*([\s\S]*?)\s*```', replies[0])
             if json_match is None:
@@ -54,29 +49,33 @@ class OutputValidator:
             replies[0] = json_match.group(1)
             self.pydantic_model.parse_obj(output_dict)
             
-            # print(
-            #     Fore.GREEN
-            #     + f"OutputValidator at Iteration {self.iteration_counter}: Valid JSON from LLM - No need for looping: {replies[0]}"
-            # )
+            print(
+                Fore.GREEN
+                + f"OutputValidator at Iteration {self.iteration_counter}: Valid JSON from LLM - No need for looping: {replies[0]}"
+            )
             return {"valid_replies": replies}
 
-        # Handle invalid JSON or other errors
         except (ValueError, ValidationError) as e:
-            # print(
-            #     Fore.RED
-            #     + f"OutputValidator at Iteration {self.iteration_counter}: Invalid JSON from LLM - Let's try again.\n"
-            #     f"Output from LLM:\n {replies[0]} \n"
-            #     f"Error from OutputValidator: {e}"
-            # )
+            print(
+                Fore.RED
+                + f"OutputValidator at Iteration {self.iteration_counter}: Invalid JSON from LLM - Let's try again.\n"
+                f"Output from LLM:\n {replies[0]} \n"
+                f"Error from OutputValidator: {e}"
+            )
             return {"invalid_replies": replies, "error_message": str(e)}
 
-output_validator = OutputValidator(pydantic_model=audioDescriptionList)
+output_validator = OutputValidator(pydantic_model=audioDescription)
+
+summary_validator = OutputValidator(pydantic_model=videoSummary)
 
 
 
 prompt_template = """
-你是專業的口述影像搞生成器，參考以下影片
-僅依照提供的資料，創建一個口述影像腳本JSON文件，儘量貼近原作品再現的原則，著重於劇情相關的畫面描述。無須描述對話，其中包含一個audioDescriptions列表，每個audioDescriptions對象包含一個時間戳(hh:mm:ss.ms)和繁體中文口述影像。時間戳應該是一個日期時間對象，內容應該是一個字符串。例如：
+影片摘要:
+{{ summary }}
+
+你是專業的口述影像稿生成器，參考影片及影片摘要。
+僅依照提供的資料，創建一個50字以內的口述影像腳本JSON文件，儘量貼近原作品再現的原則，著重於劇情相關的畫面描述。無須描述對話，其中包含一個繁體中文口述影像旁白腳本。內容應該是一個字符串。例如：
 {{schema}}
 僅使用提供的資料，不要添加任何其他資訊並確保您的答案符合格式要求，確保回覆是dict類型。
 {% if invalid_replies and error_message %}
@@ -88,6 +87,19 @@ prompt_template = """
 
 prompt_builder = PromptBuilder(template=prompt_template)
 
+summary_template = """
+用 who、what、when、where、what、how、why 摘要以下影片
+僅依照提供的資料，創建一個JSON文件，其中包含who、what、when、where、what、how、why 字段。內容應該是一個字符串。例如：
+{{schema}}
+僅使用提供的資料，不要添加任何其他資訊並確保您的答案符合格式要求，確保回覆是dict類型。
+{% if invalid_replies and error_message %}
+您已經在先前的嘗試中建立了以下輸出：{{invalid_replies}}
+但是，這不符合上面的格式要求並觸發了此 Python 異常：{{error_message}}
+更正輸出並重試。只需返回正確的輸出，無需任何額外的解釋。
+{% endif %}
+"""
+
+summary_prompt_builder = PromptBuilder(template=summary_template)
 
 @component
 class AddVideo2Prompt:
@@ -97,14 +109,14 @@ class AddVideo2Prompt:
     #     ),
     #     prompt
     # ]
-    def __init__(self, prompt: str):
-        self.prompt = prompt
 
     @component.output_types(prompt=list)
     def run(self, uri: str, prompt: str):
         return {"prompt": [Part.from_uri(uri, mime_type="video/mp4"),prompt]}
 
-add_video_2_prompt = AddVideo2Prompt(prompt=prompt_builder)
+add_video_2_prompt = AddVideo2Prompt()
+
+add_video_2_summary_prompt = AddVideo2Prompt()
 
 @component
 class GeminiGenerator:
@@ -120,6 +132,9 @@ class GeminiGenerator:
 
 gemini_generator = GeminiGenerator(project_id="gemini-rain-py", location="us-central1", model="gemini-1.5-pro-preview-0514")
 
+summary_gemini_generator = GeminiGenerator(project_id="gemini-rain-py", location="us-central1", model="gemini-1.5-flash-001")
+
+from google.cloud import storage
 @component
 class upload2GCS:
     def __init__(self, bucket_name: str):
@@ -134,37 +149,41 @@ class upload2GCS:
         blob.upload_from_filename(file_path)
         return {"uri": f"gs://{self.bucket_name}/{file_name}"}
     
-
-
 upload2gcs = upload2GCS(bucket_name="gemini-ad-gen")
 
+from haystack import Pipeline
 
 pipeline = Pipeline(max_loops_allowed=5)
-
-# Add components to your pipeline
-# pipeline.add_component(instance=prompt_builder, name="prompt_builder")
 pipeline.add_component(instance=upload2gcs, name="upload2gcs")
+pipeline.add_component(instance=summary_prompt_builder, name="summary_prompt_builder")
+pipeline.add_component(instance=add_video_2_summary_prompt, name="add_video_2_summary_prompt")
+pipeline.add_component(instance=summary_gemini_generator, name="summary_generator")
+pipeline.add_component(instance=summary_validator, name="summary_validator")
 pipeline.add_component(instance=prompt_builder, name="prompt_builder")
 pipeline.add_component(instance=add_video_2_prompt, name="add_video")
 pipeline.add_component(instance=gemini_generator, name="llm")
 pipeline.add_component(instance=output_validator, name="output_validator")
 
-# Now, connect the components to each other
-# pipeline.connect("prompt_builder", "add_video")
 pipeline.connect("upload2gcs", "add_video")
+pipeline.connect("upload2gcs", "add_video_2_summary_prompt")
+pipeline.connect("summary_prompt_builder", "add_video_2_summary_prompt")
+pipeline.connect("add_video_2_summary_prompt", "summary_generator")
+pipeline.connect("summary_generator", "summary_validator")
+pipeline.connect("summary_validator.valid_replies", "prompt_builder.summary")
+pipeline.connect("summary_validator.invalid_replies", "summary_prompt_builder")
+pipeline.connect("summary_validator.error_message", "summary_prompt_builder")
 pipeline.connect("prompt_builder", "add_video")
 pipeline.connect("add_video.prompt", "llm")
-# pipeline.connect("prompt_builder", "llm")
 pipeline.connect("llm", "output_validator")
-# # If a component has more than one output or input, explicitly specify the connections:
 pipeline.connect("output_validator.invalid_replies", "prompt_builder.invalid_replies")
 pipeline.connect("output_validator.error_message", "prompt_builder.error_message")
 
-
-path = argv[1]
+video_path = argv[1]
 output_path = argv[2]
+
 result = pipeline.run({
-    "upload2gcs": { "file_path": path},
+    "upload2gcs": { "file_path": video_path},
+    "summary_prompt_builder": {"schema": summary_schema},
     "prompt_builder": {"schema": json_schema}
 })
 
@@ -173,7 +192,3 @@ valid_json = json.loads(valid_reply)
 
 with open(output_path , "w") as f:
     json.dump(valid_json, f, indent=2)
-
-
-
-
